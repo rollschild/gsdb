9124f56fa09c9ec2503173538a48a498912915c1
rollschild <rollschild@protonmail.com>
2026-03-14 17:39:03 -0400

    initial project setup

---
+++ include/libgsdb/libgsdb.hpp
@@ -0,0 +1,8 @@
+#ifndef GSDB_LIBSDB_HPP
+#define GSDB_LIBSDB_HPP
+
+namespace gsdb {
+void say_hello();
+}
+
+#endif
---
+++ src/libgsdb.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <libgsdb/libgsdb.hpp>
+
+void gsdb::say_hello() { std::cout << "heyy" << std : endl; }
